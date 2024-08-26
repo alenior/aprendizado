f4fb04efe09fbcb7ebe9f1e8f223e5e8dc889020
alenior <alenior@gmail.com>
2024-08-26 19:58:56 -0300

    Configuração de testes.

--- flask-tutorial/tests/data.sql
+++ flask-tutorial/tests/data.sql
@@ -10,7 +10,7 @@ VALUES (
 INSERT INTO post (title, body, author_id, created)
 VALUES (
         'test title',
-        'test' || x '0a' || 'body',
+        'test body',                                  -- Original: 'test' || x'0a' || 'body',
         1,
         '2018-01-01 00:00:00'
     );
